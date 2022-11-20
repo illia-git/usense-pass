49da4a980547cf58efc30cfd3c20c8e5c2308088
illia-git <ichykurov@gmail.com>
2022-11-20 18:35:38 +0100

    initial commit

---
+++ my-app/src/pages/Home.page.tsx
@@ -0,0 +1,15 @@
+import Header from '../layout/Header';
+import Main from '../layout/Main';
+import Footer from '../layout/Footer';
+
+function Home(){
+    return (
+        <>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </>
+    )
+}
+
+export default Home;
